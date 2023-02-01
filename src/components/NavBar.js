3290aeab972608e98288d8bf4f06c6feea248074
Justin Elue <mjuelue@gmail.com>
2023-02-01 13:26:12 -0600

    Double Curly brackets Needed for Interpolation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
-import React from "react";
-
 function NavBar() {
-  const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+const links = ["home", "about", "projects"];
+const linkBits = links.map((link) => {
+  return <a key = {link} href={`#${link}`}>{link}</a>
+  })
+console.log(linkBits);
+ return (
+  <nav>
+    {linkBits} 
+  </nav>);
 }
 
 export default NavBar;
+ 
+//  return <nav>
+//     <a key = {links [0]} href="#home">home</a>
+//     <a href="#about">about</a>
+//     <a href="#projects">projects</a>
+//   </nav>;
+// }
